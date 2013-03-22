449a2e1a544b3ed1bb7f9104c86b85f146089e0a
sleepingkingstudios <merlin@sleepingkingstudios.com>
2013-03-21 20:30:51 -0400

    Runs in O(sqrt N) instead of O(N)
    - Finished in 0.00576 seconds
    - 24 examples, 0 failures

--- lib/ext/integer.rb
+++ lib/ext/integer.rb
@@ -4,12 +4,13 @@ class Integer
   def prime_factors
     return [] if self < 2
     
-    base, factors, index, step = self, [], 1, 2
+    base, factors, index, step, limit = self, [], 1, 2, Math.sqrt(self)
     (factors << 2; base >>= 1) while 0 == (base & 1)
-    while (index += step) < self
+    while (index += step) <= limit
       (factors << index; base /= index) while 0 == base % index
     end # while
     
-    factors.empty? ? [self] : factors
+    factors << base if base >= limit
+    factors
   end # method factors
 end # class
