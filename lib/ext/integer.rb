193888cd25f707ef3281c4f111ef9a79115c3208
sleepingkingstudios <merlin@sleepingkingstudios.com>
2013-03-21 20:35:24 -0400

    Minor optimization
    - Finished in 0.00546 seconds
    - 24 examples, 0 failures

--- lib/ext/integer.rb
+++ lib/ext/integer.rb
@@ -4,9 +4,13 @@ class Integer
   def prime_factors
     return [] if self < 2
     
-    base, factors, index, step, limit = self, [], 1, 2, Math.sqrt(self)
+    base, factors, index, step, limit = self, [], 1, 4, Math.sqrt(self)
+    
     (factors << 2; base >>= 1) while 0 == (base & 1)
+    (factors << 3; base /=  3) while 0 == (base % 3)
+    
     while (index += step) <= limit
+      step = step == 2 ? 4 : 2
       (factors << index; base /= index) while 0 == base % index
     end # while
     
